856eff087bdb721a217e6c329c96bdcca0d13ffb
luciano cardozo casariego <lucianoc.c117@gmail.com>
2021-09-28 16:04:19 -0300

    Lista la pagina NotFound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,23 @@
+import { useHistory } from "react-router-dom";
+import "../styles/notFound.scss";
 
 const NotFound = () => {
-  return (<h3>Not found</h3>);
+  const history = useHistory();
+  const goBackBTN = () => {
+    history.goBack();
+  };
+
+  return (
+    <div className="notFound">
+      <div className="notFound_conteiner">
+        <p className="notFound_mainStatus">404</p>
+        <p className="notFound_subtitle">Page not found</p>
+        <p className="notFound_btn" onClick={goBackBTN}>
+          Go back
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
